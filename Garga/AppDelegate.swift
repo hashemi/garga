059272beb91089472718989caadae27997430bdb
Ahmad Alhashemi <ahmad@ahmadh.com>
2019-03-15 12:35:25 +0300

    Initial Commit

---
+++ Garga/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Garga
+//
+//  Created by Ahmad Alhashemi on 2019-03-15.
+//  Copyright © 2019 Ahmad Alhashemi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Garga/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Garga
+//
+//  Created by Ahmad Alhashemi on 2019-03-15.
+//  Copyright © 2019 Ahmad Alhashemi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
